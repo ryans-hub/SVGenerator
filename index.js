afad9c21ba10bce4f8b4d334bf8014e2f142a77e
PanMan <ryanispan@gmail.com>
2024-03-07 18:13:45 -0700

    updating lib to have seperate js to handle cli class, updating shapes.text.js and shapes.js for successful test

--- index.js
+++ index.js
@@ -1,35 +1,41 @@
-const inquirer = require("inquirer");
+// const inquirer = require("inquirer");
 
-const questions = [
-    {
-        type: 'input',
-        message: 'Enter up to 3 characters',
-        name: 'text',
-    },
-    {
-        type: 'input',
-        message: 'Enter a color keyword for the text (OR a hexadecimal number)',
-        name: 'textColor',
-    },
-    {
-        type: 'list',
-        message: 'Choose a shape',
-        choices: ['Circle', 'Triangle', 'Square'],
-        name: 'shape',
-    },
-    {
-        type: 'input',
-        message: 'Enter a color keyword for the shape (OR a hexadecimal number)',
-        name: 'shapeColor',
-    },
-];
+// const questions = [
+//     {
+//         type: 'input',
+//         message: 'Enter up to 3 characters',
+//         name: 'text',
+//     },
+//     {
+//         type: 'input',
+//         message: 'Enter a color keyword for the text (OR a hexadecimal number)',
+//         name: 'textColor',
+//     },
+//     {
+//         type: 'list',
+//         message: 'Choose a shape',
+//         choices: ['Circle', 'Triangle', 'Square'],
+//         name: 'shape',
+//     },
+//     {
+//         type: 'input',
+//         message: 'Enter a color keyword for the shape (OR a hexadecimal number)',
+//         name: 'shapeColor',
+//     },
+// ];
 
-function init() {
-    inquirer.prompt(questions)
-    .then((response) => {
-        console.log('Inputs:', response);
+// function init() {
+//     inquirer.prompt(questions)
+//     .then((response) => {
+//         console.log('Inputs:', response);
 
-    })
-};
+//     })
+// };
 
-init();
+// init();
+
+const CLI = require('./lib/cli');
+
+const cli = new CLI();
+
+cli.start();
